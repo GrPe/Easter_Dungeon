772d9393bece817c312d2029ca08278446899677
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2019-05-02 13:46:34 +0200

    Added player movement Script

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent agent;
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
