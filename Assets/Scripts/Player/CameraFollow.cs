03da613598a52cc9568e1d598ef45d8ccdb31a82
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2019-05-02 13:51:37 +0200

    Added Camera Follower

---
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private GameObject target = null;
+    [SerializeField] private float distanceFromTarget = 10f;
+    
+    void Start()
+    {
+        transform.position = target.transform.position + new Vector3(0, distanceFromTarget, 0);
+        transform.rotation = Quaternion.Euler(90, 0, 0);
+    }
+    
+    void Update()
+    {
+        transform.position = target.transform.position + new Vector3(0, distanceFromTarget, 0);
+    }
+}
