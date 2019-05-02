767252d666072b97f8ef547bdd487aeb9b960332
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2019-05-02 14:46:59 +0200

    Added base FSM

---
+++ Assets/Scripts/FSM/State.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class State
+{
+    protected Dictionary<Transition, StateID> map = new Dictionary<Transition, StateID>();
+
+    public StateID StateID { get; protected set; }
+
+    public void AddTransition(Transition trans, StateID id)
+    {
+        // Check if anyone of the args is invalid
+        if (trans == Transition.NullTransition)
+        {
+            Debug.LogError("FSMState ERROR: NullTransition is not allowed for a real transition");
+            return;
+        }
+
+        if (id == StateID.NullStateID)
+        {
+            Debug.LogError("FSMState ERROR: NullStateID is not allowed for a real ID");
+            return;
+        }
+
+        // Since this is a Deterministic FSM,
+        //   check if the current transition was already inside the map
+        if (map.ContainsKey(trans))
+        {
+            Debug.LogError($"FSMState ERROR: State {StateID.ToString()} already has transition {trans.ToString()} " +
+                           "Impossible to assign to another state");
+            return;
+        }
+
+        map.Add(trans, id);
+    }
+
+    public void DeleteTransition(Transition trans)
+    {
+        // Check for NullTransition
+        if (trans == Transition.NullTransition)
+        {
+            Debug.LogError("FSMState ERROR: NullTransition is not allowed");
+            return;
+        }
+
+        // Check if the pair is inside the map before deleting
+        if (map.ContainsKey(trans))
+        {
+            map.Remove(trans);
+            return;
+        }
+        Debug.LogError($"FSMState ERROR: Transition {trans.ToString()} passed to {StateID.ToString()}" +
+                       " was not on the state's transition list");
+    }
+
+    public StateID GetOutputState(Transition trans)
+    {
+        // Check if the map has this transition
+        if (map.ContainsKey(trans))
+        {
+            return map[trans];
+        }
+        return StateID.NullStateID;
+    }
+
+    public virtual void DoBeforeEntering() { }
+
+    public virtual void DoBeforeLeaving() { }
+
+    public virtual void Act() { }
+
+    public virtual void OnCollision2DEnter(Collision2D collision) { }
+
+    public virtual void OnCollision2DStay(Collision2D collision) { }
+
+    public virtual void OnTrigger2DEnter(Collider2D collider) { }
+
+    public virtual void OnTrigger2DStay(Collider2D collider) { }
+
+    public virtual void OnTrigger3DEnter(Collider collider) { }
+
+    public virtual void OnTrigger3DStay(Collider collider) { }
+}
---
+++ Assets/Scripts/FSM/StateMachine.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine
+{
+    private List<State> states;
+    public StateID CurrentStateID { get; private set; }
+    public State CurrentState { get; private set; }
+
+    public StateMachine()
+    {
+        states = new List<State>();
+    }
+
+    public void AddState(State s)
+    {
+
+        // Check for Null reference before deleting
+        if (s == null)
+        {
+            Debug.LogError("FSM ERROR: Null reference is not allowed");
+        }
+
+        // First State inserted is also the Initial state,
+        //   the state the machine is in when the simulation begins
+        if (states.Count == 0)
+        {
+            states.Add(s);
+            CurrentState = s;
+            CurrentStateID = s.StateID;
+            CurrentState.DoBeforeEntering();
+            return;
+        }
+
+        // Add the state to the List if it's not inside it
+        foreach (State state in states)
+        {
+            if (state.StateID == s.StateID)
+            {
+                Debug.LogError($"FSM ERROR: Impossible to add state {s.StateID.ToString()} because state has already been added");
+                return;
+            }
+        }
+        states.Add(s);
+    }
+
+
+    public void DeleteState(StateID id)
+    {
+        // Check for NullState before deleting
+        if (id == StateID.NullStateID)
+        {
+            Debug.LogError("FSM ERROR: NullStateID is not allowed for a real state");
+            return;
+        }
+
+        // Search the List and delete the state if it's inside it
+        foreach (State state in states)
+        {
+            if (state.StateID == id)
+            {
+                states.Remove(state);
+                return;
+            }
+        }
+        Debug.LogError($"FSM ERROR: Impossible to delete state {id.ToString()}. It was not on the list of states");
+    }
+
+    public void PerformTransition(Transition trans)
+    {
+        // Check for NullTransition before changing the current state
+        if (trans == Transition.NullTransition)
+        {
+            Debug.LogError("FSM ERROR: NullTransition is not allowed for a real transition");
+            return;
+        }
+
+        // Check if the currentState has the transition passed as argument
+        StateID id = CurrentState.GetOutputState(trans);
+        if (id == StateID.NullStateID)
+        {
+            Debug.LogError($"FSM ERROR: State {CurrentStateID.ToString()} does not have a target state " +
+                           $" for transition {trans.ToString()}");
+            return;
+        }
+
+        // Update the currentStateID and currentState		
+        CurrentStateID = id;
+        foreach (State state in states)
+        {
+            if (state.StateID == CurrentStateID)
+            {
+                // Do the post processing of the state before setting the new one
+                CurrentState.DoBeforeLeaving();
+
+                CurrentState = state;
+
+                // Reset the state to its desired condition before it can reason or act
+                CurrentState.DoBeforeEntering();
+                break;
+            }
+        }
+
+    }
+}
