c64afe392de472d21b79d173d5e9511c0b78f2e2
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2019-05-11 13:19:09 +0200

    Added EndPoint

---
+++ Assets/Scripts/EndPoint/EndPoint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndPoint : MonoBehaviour
+{
+    [SerializeField] private GameObject particle;
+    [SerializeField] private bool isActive;
+
+    public event Action OnPlayerWin;
+
+    public void Activate()
+    {
+        isActive = true;
+
+        if(particle != null)
+        {
+            Instantiate(particle, gameObject.transform);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && isActive == true)
+        {
+            OnPlayerWin?.Invoke();
+        }
+    }
+}
