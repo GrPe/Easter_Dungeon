99d806b5dc22619d7943e230792b198a222a81d7
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2019-05-11 13:39:03 +0200

    Added game win screen

---
+++ Assets/Scripts/Managers/WinManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinManager : MonoBehaviour
+{
+    [SerializeField] private Image gameWinMenu;
+    [SerializeField] private EndPoint endPoint;
+
+    private void Start()
+    {
+        endPoint.OnPlayerWin += PlayerWin;
+        gameWinMenu.gameObject.SetActive(false);
+    }
+
+    private void PlayerWin()
+    {
+        gameWinMenu.gameObject.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void OnButtonMenuClick()
+    {
+
+    }
+
+    public void OnButtonNextClick()
+    {
+
+    }
+}
