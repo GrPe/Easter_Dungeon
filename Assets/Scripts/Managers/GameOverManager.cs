4b43fb5b76be3cd7d64718f47065a50527a121db
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2019-05-11 12:44:37 +0200

    Added game over screen

---
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverManager : MonoBehaviour
+{
+    [SerializeField] private Image gameOverMenu;
+    [SerializeField] private Player player;
+
+    private void Start()
+    {
+        gameOverMenu.gameObject.SetActive(false);
+        player.OnPlayerDie += PlayerDie;
+    }
+
+    private void PlayerDie()
+    {
+        gameOverMenu.gameObject.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void OnButtonMenuClick()
+    {
+
+    }
+
+    public void OnButtonRetryClick()
+    {
+
+    }
+}
