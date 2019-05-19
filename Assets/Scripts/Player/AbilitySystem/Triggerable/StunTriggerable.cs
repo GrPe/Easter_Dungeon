45008799818a8ad971f8bcf3efb050ea5d8f56d3
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2019-05-19 13:33:36 +0200

    Added stun ability (not tested and connected)

---
+++ Assets/Scripts/Player/AbilitySystem/Triggerable/StunTriggerable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StunTriggerable : MonoBehaviour
+{
+    [HideInInspector] public float Duration;
+    [HideInInspector] public float Range;
+
+    public void Invoke()
+    {
+        var colliders = Physics.OverlapSphere(transform.position, Range);
+
+        foreach(var collider in colliders)
+        {
+            if(collider.tag == "Enemy")
+            {
+                var enemy = collider.gameObject.GetComponent<Enemy>();
+                enemy.Stun(Duration);
+            }
+        }
+    }
+}
