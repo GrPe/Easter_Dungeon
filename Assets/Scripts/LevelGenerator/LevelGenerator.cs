c1c608a5029979b5322bb237f2cf43ea49fa9129
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2019-05-25 19:24:19 +0200

    Added column generation and created prefab of levelGenerator

--- Assets/Scripts/LevelGenerator/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator/LevelGenerator.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,6 +14,9 @@ public class LevelGenerator : MonoBehaviour
 
     [SerializeField] private Vector2 wallSize;
     [SerializeField] private GameObject wall;
+
+    [SerializeField] private GameObject column;
+    [SerializeField] private Vector2 columnSize;
 
     public bool ok = false;
 
@@ -38,6 +40,7 @@ public class LevelGenerator : MonoBehaviour
                 {
                     var instance = Instantiate(floor, new Vector3(i * floorSize.x, 0, j * floorSize.y), Quaternion.identity, transform);
                     CreateWalls(maze, i, j, instance.transform);
+                    CreateColumn(maze, i, j, instance.transform);
                 }
             }
         }
@@ -45,25 +48,72 @@ public class LevelGenerator : MonoBehaviour
 
     private void CreateWalls(bool[,] maze, int x, int y, Transform parent)
     {
-        //up
+
         if (!maze[x + 1, y] || x + 1 == maze.GetLength(0) - 1)
         {
             Instantiate(wall, new Vector3(x * wallSize.x + wallSize.x, 0, y * wallSize.y + wallSize.y), Quaternion.Euler(0, 180, 0), parent);
         }
-        //down
+
         if (!maze[x - 1, y] || x - 1 == 0)
         {
             Instantiate(wall, new Vector3(x * wallSize.x, 0, y * wallSize.y), Quaternion.Euler(0, 0, 0), parent);
         }
-        //right ++
+
         if (!maze[x, y + 1] || y + 1 == maze.GetLength(1) - 1)
         {
             Instantiate(wall, new Vector3(x * wallSize.x, 0, y * wallSize.y + wallSize.y), Quaternion.Euler(0, 90, 0), parent);
         }
-        //left 
+
         if (!maze[x, y - 1] || y - 1 == 0)
         {
             Instantiate(wall, new Vector3(x * wallSize.x + wallSize.x, 0, y * wallSize.y), Quaternion.Euler(0, 270, 0), parent);
+        }
+    }
+
+    private void CreateColumn(bool[,] maze, int x, int y, Transform parent)
+    {
+        //up-right
+        if (!maze[x - 1, y] && !maze[x, y + 1])
+        {
+            Instantiate(column, new Vector3(x * columnSize.x, 0, y * columnSize.y + columnSize.y), Quaternion.identity, parent);
+        }
+        //down-right
+        if (!maze[x + 1, y] && !maze[x, y + 1])
+        {
+            Instantiate(column, new Vector3(x * columnSize.x + columnSize.x*(4f/3f), 0, y * columnSize.y + columnSize.y), Quaternion.identity, parent);
+        }
+        //up-left
+        if (!maze[x - 1, y] && !maze[x, y - 1])
+        {
+            Instantiate(column, new Vector3(x * columnSize.x, 0, y * columnSize.y - columnSize.y/3f), Quaternion.identity, parent);
+        }
+        //down-left
+        if (!maze[x + 1, y] && !maze[x, y - 1])
+        {
+            Instantiate(column, new Vector3(x * columnSize.x + columnSize.x * (4f / 3f), 0, y * columnSize.y - columnSize.y / 3f), Quaternion.identity, parent);
+        }
+
+        //reverse
+
+        //up-right
+        if (maze[x - 1, y] && maze[x, y + 1] && !maze[x - 1, y + 1])
+        {
+            Instantiate(column, new Vector3(x * columnSize.x, 0, y * columnSize.y + columnSize.y), Quaternion.identity, parent);
+        }
+        //down-right
+        if (maze[x + 1, y] && maze[x, y + 1] && !maze[x + 1, y + 1])
+        {
+            Instantiate(column, new Vector3(x * columnSize.x + columnSize.x * (4f / 3f), 0, y * columnSize.y + columnSize.y), Quaternion.identity, parent);
+        }
+        //up-left
+        if (maze[x - 1, y] && maze[x, y - 1] && !maze[x -1, y-1])
+        {
+            Instantiate(column, new Vector3(x * columnSize.x, 0, y * columnSize.y - columnSize.y / 3f), Quaternion.identity, parent);
+        }
+        //down-left
+        if (maze[x + 1, y] && maze[x, y - 1] && !maze[x + 1, y -1])
+        {
+            Instantiate(column, new Vector3(x * columnSize.x + columnSize.x * (4f / 3f), 0, y * columnSize.y - columnSize.y / 3f), Quaternion.identity, parent);
         }
     }
 }
