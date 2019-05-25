1de5bf21e8d160b49e69795c313663ebf71ceb7d
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2019-05-25 13:02:12 +0200

    Added LevelGenerator (use assets)

---
+++ Assets/Scripts/LevelGenerator/LevelGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class LevelGenerator : MonoBehaviour
+{
+    [SerializeField] private AutomataRule rule;
+    [SerializeField] private int fulfillment;
+    [SerializeField] private Vector2 levelSize;
+
+    [SerializeField] private Vector2 floorSize;
+    [SerializeField] private GameObject floor;
+
+
+    private void Awake()
+    {
+        Generator generator = new CellularAutomataGenerator(rule, fulfillment);
+
+        var maze = generator.GenerateMaze((int)levelSize.x, (int)levelSize.y);
+
+        for (int i = 0; i < maze.GetLength(0); i++)
+        {
+            for (int j = 0; j < maze.GetLength(1); i++)
+            {
+                Instantiate(floor, new Vector3(i * floorSize.x, 0, j * floorSize.y), Quaternion.identity);
+            }
+        }
+    }
+}
