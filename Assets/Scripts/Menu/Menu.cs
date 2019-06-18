b0cdd8fd0b2dbdfc49b2b14fc53975f67e6dfcd6
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2019-06-19 00:51:15 +0200

    Added menu scripts and moved menu scene

---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public void OnSelectLevelClick()
+    {
+        //todo
+    }
+
+    public void OnCreditsClick()
+    {
+        //todo
+    }
+
+    public void OnExitClick()
+    {
+        Application.Quit();
+    }
+
+    public void OnReturnButtonClick()
+    {
+        //todo
+    }
+}
