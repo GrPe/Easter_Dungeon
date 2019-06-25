f212fead1ab7af30ec1b332dff266a3551736243
Grzegorz Perzanowski <grzesiek.p9804@gmail.com>
2019-06-25 14:57:42 +0200

    Added sneaking effect to player

--- Assets/Scripts/Player/AbilitySystem/Triggerable/SneakingTriggerable.cs
+++ Assets/Scripts/Player/AbilitySystem/Triggerable/SneakingTriggerable.cs
@@ -4,23 +4,35 @@ using UnityEngine;
 
 public class SneakingTriggerable : MonoBehaviour
 {
+    [SerializeField] private GameObject model;
+    private Renderer modelRenderer = null;
+    private Color backupColor;
+
     public bool IsHidden { get; private set; }
     [HideInInspector] public float Duration;
     private float timeLeft = 0f;
 
     public void Invoke()
     {
+        if(modelRenderer == null)
+        {
+            modelRenderer = model.GetComponent<Renderer>();
+        }
+
         timeLeft = Duration;
         IsHidden = true;
+        backupColor = modelRenderer.material.color;
+        modelRenderer.material.color = new Color(1, 0.5f, 1, 0.5f);
     }
 
     public void Update()
     {
         timeLeft -= Time.deltaTime;
-        if(timeLeft <=0)
+        if(timeLeft <=0 && IsHidden)
         {
             //clear;
             IsHidden = false;
+            modelRenderer.material.color = backupColor;
         }
     }
 }
